3007eeca3b5ae922a12d397d78d58a8bd8f92875
MaryKwawu <may419316@gmail.com>
2024-10-09 13:07:39 +0000

    feat/StudentDatabase : created attributes for the student class

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,4 +1,20 @@
 package studentdatabaseapp;
 
 public class Student {
+    String firstName;
+    String secondName;
+    int studentGradeYear;
+    int studentId;
+    String[] studentCourses;
+    int tuitionBalance;
+    int studentTuition = 600;
+    String studentStatus;
+
+
+    //constructor prompt user to enter student's name and year
+    //Generate an id
+    //Enroll in courses
+    //view the balance
+    //pay tuition
+    //show status
 }
