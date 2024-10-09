8b055e85a7de70adb53cae82328054f5914cce40
MaryKwawu <may419316@gmail.com>
2024-10-09 13:18:11 +0000

    feat/StudentDatabaseApp : added main to StudentDatabaseApp

--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,5 +1,12 @@
 package studentdatabaseapp;
 
 public class StudentDatabaseApp {
+    public static void main(String[] arg){
+        //Ask how many users we want to add
+
+
+        //create n number of students
+
+    }
 
 }
