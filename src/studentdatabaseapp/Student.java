41468af5a3a5db9d5f7695dcc04c928f2eb6b92d
MaryKwawu <may419316@gmail.com>
2024-10-09 14:27:27 +0000

    feat/constructor : created a constructor for students

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,18 +1,35 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
     String firstName;
     String secondName;
     int studentGradeYear;
     int studentId;
     String[] studentCourses;
-    int tuitionBalance;
-    int studentTuition = 600;
-    String studentStatus;
+    int studentTuitionBalance;
+    static int costOfCourse = 600;
+    static int id = 2000;
+   // String studentStatus;
 
 
     //constructor prompt user to enter student's name and year
-    //Generate an id
+    public Student () {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Enter student first name: ");
+        this.firstName = scanner.nextLine();
+
+        System.out.println("Enter student second name: ");
+        this.secondName = scanner.nextLine();
+
+        System.out.println("1 - Freshmen\n2 - for  Sophomore\n3 - Junior\n4 - Senior\n5 Enter student class level: ");
+        this.studentGradeYear = scanner.nextInt();
+        System.out.println(firstName + " " + secondName + " " + studentGradeYear);
+    }
+
     //Enroll in courses
     //view the balance
     //pay tuition
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -2,6 +2,7 @@ package studentdatabaseapp;
 
 public class StudentDatabaseApp {
     public static void main(String[] arg){
+        Student student = new Student();
         //Ask how many users we want to add
 
 
