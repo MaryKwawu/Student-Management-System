47794b1977b58059bf3aa4b0263f3f15d7d66277
MaryKwawu <may419316@gmail.com>
2024-10-18 07:57:15 +0000

    feat/createnumberofstrudenttoenroll : created a for a loop for the number of student to enroll

--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,14 +1,27 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
     public static void main(String[] arg){
-        Student student1 = new Student();
-        student1.enroll();
-        student1.payTuition();
-        System.out.println(student1.toString());
+
+
+
         //Ask how many users we want to add
+        System.out.println("Enter number of new students to enroll: ");
+        Scanner scanner = new Scanner(System.in);
+        int numOfStudents = scanner.nextInt();
+        Student[] students = new Student[numOfStudents];
 
         //create n number of students
+        for(int n = 0; n < numOfStudents; n++){
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+        }
+        for(int n = 0; n < numOfStudents; n++) {
+            System.out.println(students[n].toString());
+        }
 
     }
 
